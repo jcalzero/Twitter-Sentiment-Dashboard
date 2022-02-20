1b2b03fba2c15547c7eda2867ec90d3e215ddf9c
Julien Calfayan <julien.9@hotmail.com>
2022-02-20 14:51:47 -0800

    Update Sidebar.js

--- template/demo_1/src/app/shared/Sidebar.js
+++ template/demo_1/src/app/shared/Sidebar.js
@@ -59,19 +59,6 @@ class Sidebar extends Component {
     return (
       <nav className="sidebar sidebar-offcanvas" id="sidebar">
         <ul className="nav">
-          <li className="nav-item nav-profile">
-            <a href="!#" className="nav-link" onClick={evt =>evt.preventDefault()}>
-              <div className="nav-profile-image">
-                <img src={ require("../../assets/images/faces/face1.jpg") } alt="profile" />
-                <span className="login-status online"></span> {/* change to offline or busy as needed */}
-              </div>
-              <div className="nav-profile-text">
-                <span className="font-weight-bold mb-2"><Trans>David Grey. H</Trans></span>
-                <span className="text-secondary text-small"><Trans>Project Manager</Trans></span>
-              </div>
-              <i className="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-            </a>
-          </li>
           <li className={ this.isPathActive('/dashboard') ? 'nav-item active' : 'nav-item' }>
             <Link className="nav-link" to="/dashboard">
               <span className="menu-title"><Trans>Dashboard</Trans></span>
