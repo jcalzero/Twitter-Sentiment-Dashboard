d2ff6277a0432ab2151899e00d1017d43e715dc0
Julien Calfayan <julien.9@hotmail.com>
2022-02-21 10:30:00 -0800

    Sidebar and Search fixed

--- src/app/shared/Navbar.js
+++ src/app/shared/Navbar.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
 
 class Navbar extends Component {
+  state = {
+    message: ""
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+  }
 
   toggleOffcanvas() {
     document.querySelector('.sidebar-offcanvas').classList.toggle('active');
@@ -15,24 +21,33 @@ class Navbar extends Component {
 
   render () {
     return (
-      <nav className="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <nav className="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row flex-nowrap justify-content-start">
         <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
           <img src={require('../../assets/images/TwitSent.png')} alt="logo" width={50} length={50}/>
         </div>
-        <div className="navbar-menu-wrapper d-flex align-items-stretch">
+        <div className="navbar-menu-wrapper d-flex w-100 align-items-stretch">
           <button className="navbar-toggler navbar-toggler align-self-center" type="button" onClick={ () => document.body.classList.toggle('sidebar-icon-only') }>
             <span className="mdi mdi-menu"></span>
           </button>
-          <div className="search-field d-none d-md-block w-100">
-            <form className="d-flex align-items-center h-100" onSubmit={this.handleSubmit}>
+          <div className="search-field d-md-block w-100">
+            <form className="d-flex align-items-center h-100" onSubmit={this.submitHandler}>
               <div className="input-group">
                 <div className="input-group-prepend bg-transparent">
                   <i className="input-group-text border-0 mdi mdi-magnify"></i>
                 </div>
-                <Form inline>
-                  <Form.Control type="text" placeholder="Search keywork" className="bg-transparent border-0" />
-                </Form>
-                {/* <input type="text" className="form-control bg-transparent border-0" placeholder="Search keyword"/> */}
+                <input
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      this.setState({ message: e.target.value },
+                      () => {
+                          console.log(this.state.message);
+                      });
+                    }
+                  }}
+                  type="text"
+                  className="form-control bg-transparent border-0"
+                  placeholder="Search keyword"
+                />
               </div>
             </form>
           </div>
