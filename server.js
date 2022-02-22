6b7b3fad92436095713fb5cbc0c98e661b99668d
Julien Calfayan <julien.9@hotmail.com>
2022-02-21 19:05:29 -0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/",express.static(__dirname, "src", "build"));
+app.use("/",express.static(__dirname + "build"));
 
 app.post("/analyze", (request, response)=>{
   const { sentence } = request.body;
@@ -45,7 +45,7 @@ app.post("/analyze", (request, response)=>{
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "src", "build"));
+  res.sendFile(path.join(__dirname, "build"));
 });
 
 // Listen
